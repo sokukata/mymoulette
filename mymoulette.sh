c2fa0c4646779a50b446481b725313fef2070acc
Ludwig Patte <ludwig.patte@epita.fr>
2017-11-16 19:09:32 +0100

    [UPDATE] 8th level
    Signed-off-by: Ludwig Patte <ludwig.patte@epita.fr>

--- mymoulette.sh
+++ mymoulette.sh
@@ -23,6 +23,13 @@ mkdir /mnt/newroot  #debootstrap stable /mnt/newroot/ http://httpredir.debian.or
 mount -t tmpfs none /mnt/newroot
 cp -r /mnt/debootstrap/* /mnt/newroot/
 
+cp $1 /mnt/newroot/home/
+
+cp /usr/bin/unshare /mnt/newroot/usr/bin/unshare
+cp /sbin/capsh /mnt/newroot/sbin/
+
+name=$(basename $1)
+shift
 
 cmd="mount --make-rslave /;"
 
@@ -35,20 +42,27 @@ cmd=$cmd"cd /mnt/newroot;"
 cmd=$cmd"mkdir old_root;"
 cmd=$cmd"pivot_root . old_root;"
 
-cmd=$cmd"setcap cap_net_raw-epi $(which bash);"
+
+#cmd=$cmd"/sbin/setcap cap_net_raw-epi $(which $1);"
+
+#cmd=$cmd"/sbin/capsh --drop=cap_net_raw --;"
 
 cmd=$cmd"mkdir -p /sys/fs/cgroup/memory/mymoulette;"
 cmd=$cmd"mkdir -p /sys/fs/cgroup/cpu/mymoulette;"
 cmd=$cmd"mkdir -p /sys/fs/cgroup/pids/mymoulette;"
 
-cmd=$cmd"echo $$ > /sys/fs/cgroup/memory/mymoulette/tasks;"
-cmd=$cmd"echo $$ > /sys/fs/cgroup/cpu/mymoulette/tasks;"
-cmd=$cmd"echo $$ > /sys/fs/cgroup/pids/mymoulette/tasks;"
+cmd=$cmd"echo \$\$ > /sys/fs/cgroup/memory/mymoulette/tasks;"
+cmd=$cmd"echo \$\$ > /sys/fs/cgroup/cpu/mymoulette/tasks;"
+cmd=$cmd"echo \$\$ > /sys/fs/cgroup/pids/mymoulette/tasks;"
 
 cmd=$cmd"echo 1G > /sys/fs/cgroup/memory/mymoulette/memory.limit_in_bytes;"
 cmd=$cmd"echo 10000 > /sys/fs/cgroup/cpu/mymoulette/cpu.cfs_quota_us;"
 cmd=$cmd"echo 100 > /sys/fs/cgroup/pids/mymoulette/pids.max;"
 
-cmd=$cmd"exec chroot /  /bin/bash -c \"$@\";"
+cmd=$cmd"hostname $(uuidgen);"
 
-unshare -p -m -f --mount-proc /bin/bash -c "$cmd"
+cmd=$cmd"/sbin/capsh --drop=cap_net_raw --chroot=/ -- -c \"unshare -U /bin/bash -c '/home/$name $@' \";"
+
+unshare -C -i -m -n -p -u  -f --mount-proc /bin/bash -c "$cmd"
+
+rm /mnt/newroot/home/$name
